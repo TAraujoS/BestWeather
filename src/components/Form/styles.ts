4f2f54ae06f01da16bcfe210dcea45d79048c633
Larissa Sato <larissasato_@outlook.com>
2022-08-29 17:04:54 -0300

    update: fixed Form/styles.tsx

--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Form = styled.form`
+const Form = styled.form`
   display: flex;
   flex-direction: column;
   margin: auto;
@@ -29,4 +29,5 @@ export const Form = styled.form`
     color: #08263d;
   }
 `;
+
 export default Form;
