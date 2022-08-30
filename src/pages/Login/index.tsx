f214da42d498f1670f5d1d3c3e9d092f6fc7f733
Larissa Sato <larissasato_@outlook.com>
2022-08-30 14:15:09 -0300

    create: AuthContext, requires Login.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,15 +2,17 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import HeaderLogo from "../../components/Header";
 import { loginSchema } from "../../validators";
-
+import { AuthContext, ILoginProps } from "../../Context/LoginContext";
 import Form from "../../components/Form/styles";
 import { Container } from "./styles";
 import { Button } from "../../components/Button/styles";
+import { useContext } from "react";
 
 const Login = () => {
-  const { register } = useForm({
+  const { register, handleSubmit } = useForm<ILoginProps>({
     resolver: yupResolver(loginSchema),
   });
+  const { signIn } = useContext(AuthContext);
 
   return (
     <Container>
@@ -25,7 +27,7 @@ const Login = () => {
         <div className="modalLogin">
           <h3>Login</h3>
 
-          <Form>
+          <Form onSubmit={handleSubmit(signIn)}>
             <input
               type="email"
               id="email"
