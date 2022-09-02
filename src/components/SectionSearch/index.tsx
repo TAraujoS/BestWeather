ffa336e458ef03b66ae79f9a52765cf6b6cf6bb6
Daniel Carvalho <danielzarkos92@gmail.com>
2022-09-02 12:07:14 -0300

    feat: section-search functional

--- src/components/SectionSearch/index.tsx
+++ src/components/SectionSearch/index.tsx
@@ -1,10 +1,33 @@
 import { useContext } from "react";
+import { useForm } from "react-hook-form";
 import { BiSearchAlt } from "react-icons/bi";
 import { AuthContext } from "../../Context/LoginContext";
+import { weatherApi } from "../../services";
 import { SectionStyle } from "./styles";
 
+// interface IFunctions {
+//   searchFromInput: (cityValue: IData) => void;
+// }
+
+interface IData {
+  data: string;
+  city: string;
+}
+
+const key = "b4b75ed7ee9a4858bdd230139222908";
+
 const SectionSearch = () => {
+  const { register, handleSubmit } = useForm<IData>();
   const { user } = useContext(AuthContext);
+
+  const searchFromInput = async (data: IData) => {
+    console.log();
+    console.log(data.city);
+    await weatherApi
+      .get(`/current.json?key=${key}&q=${data.city}`)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
 
   return (
     <SectionStyle>
@@ -13,12 +36,16 @@ const SectionSearch = () => {
           Olá, {user.name}, como está o seu clima hoje?
         </h3>
 
-        <div>
-          <input type="text" placeholder="Digite a sua localização aqui" />
-          <button className="search-button">
+        <form onSubmit={handleSubmit(searchFromInput)}>
+          <input
+            type="text"
+            placeholder="Digite a sua localização aqui"
+            {...register("city")}
+          />
+          <button type="submit" className="search-button">
             <BiSearchAlt />
           </button>
-        </div>
+        </form>
       </>
     </SectionStyle>
   );
