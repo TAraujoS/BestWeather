633234c166cfa4197f820cfa8dc97ca60afb57d5
Larissa Sato <larissasato_@outlook.com>
2022-09-05 15:56:11 -0300

    update: component Banners

--- src/components/Banners/index.tsx
+++ src/components/Banners/index.tsx
@@ -17,11 +17,11 @@ interface IBannerResponse {
 
 interface IInfos {
   title: string;
-  text1?: string;
-  text2?: string;
-  text3?: string;
-  text4?: string;
-  text5?: string;
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+  text5: string;
   url: string;
   userId: number;
   id: number;
@@ -31,7 +31,8 @@ const Banners = () => {
   const { userLogin } = useContext(AuthContext);
   const { cityApi } = useContext(CityContext);
 
-  const [banner, setBanner] = useState<IBannerResponse>({} as IBannerResponse);
+  const [banner, setBanner] = useState<IInfos[]>([]);
+  console.log(banner);
 
   const token = localStorage.getItem("@loginBWeather:token") || "";
 
@@ -80,24 +81,24 @@ const Banners = () => {
     );
   };
 
-  const checkOccupation = (occupation: string, elem: IInfos) => {
+  const checkOccupation = (occupation: string, text: IInfos) => {
     if (occupation === "Paraquedista" || "Asa Delta") {
-      return getMessagePrec(elem);
+      return getMessagePrec(text);
     } else if (occupation === "Surfista") {
-      return getMessageWind(elem);
+      return getMessageWind(text);
     } else {
-      return getMessageTemp(elem);
+      return getMessageTemp(text);
     }
   };
 
   return (
     <Banner>
-      {banner.infos?.map((elem) => (
+      {banner.map((elem) => (
         <>
-          <div className="divText">
+          <div className="divText" key={elem.id}>
             <h3>{elem.title}</h3>
             <p>{userLogin.name},</p>
-            {checkOccupation(banner.occupation, elem)}
+            {checkOccupation(userLogin.occupation, elem)}
           </div>
           <img src={elem.url} alt="Occupation" />
         </>
